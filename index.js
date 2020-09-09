3e9613bb83f5ef85c9b667fb44b0c160c538b52a
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 15:36:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 // here we are creating generalized functions
-
 var wakeDog = function(dogName="Byron", dogBreed="poodle") {
   console.log(`Wake ${dogName} the ${dogBreed}`);
   return `Wake ${dogName} the ${dogBreed}`
