0aabd69c00fbd075f1167936b5426a917a6d94c1
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 15:50:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,4 +33,10 @@ var walkHome = function(dogName, dogBreed) {
 
 const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, unleashDog, walkHome]
 
-function exerciseDog(dogName, dogBreed, routine)
+function exerciseDog(dogName, dogBreed, routine){
+  let i = 0;
+  while (i < routine.length) {
+    routine(dogName, dogBreed);
+    i++;
+  }
+}
