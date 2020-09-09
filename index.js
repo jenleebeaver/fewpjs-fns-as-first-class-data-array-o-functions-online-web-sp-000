32d64818c58a9def2ff7c4e92b221fb851aaaef7
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-09-09 15:39:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var walkToPark = function(dogName, dogBreed) {
   return `Walk to the park with ${dogName} the ${dogBreed}`
 }
 
-var walkToPark = function(dogName, dogBreed) {
+var throwFrisbee = function(dogName, dogBreed) {
   console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
   return `Walk to the park with ${dogName} the ${dogBreed}`
 }
